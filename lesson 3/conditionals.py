d64520460a134888a1fb94eb9b9ead352b0798b2
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-03-06 12:15:49 -0300

    Lesson 3

---
+++ lesson 3/conditionals.py
@@ -0,0 +1,15 @@
+value = 10
+
+print("")
+
+# if value > 8:
+#     print("Value is greater than 8")
+# else:
+#     print("Value is not greater than 8")
+
+
+# Ternary operator
+print("Value is greater than 8") if value > 8 else print("Value is not greater than 8")
+
+
+print("")
---
+++ lesson 3/operators.py
@@ -0,0 +1,22 @@
+# Unique Python operators
+
+# // - division flooring the result if decimal
+number1 = 24 / 5 # 4.8
+number2 = 16 / 4 # 4.0
+number3 = 24 // 5 # 4 # rounds down
+number2NoDecimal = round(number2) # doesn't modify the original number, but rounds up if greater than 4.5
+
+# not boolean operator
+
+x = True
+y = False
+z = True
+
+not x # False
+not y # True
+
+# and boolean operator
+
+x and y # False
+x and z # True
+
