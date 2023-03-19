20e923503022dd4077cb23639e01c6ffb9266ce7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-03-19 11:29:07 -0300

    Create ex012.py

---
+++ exercises/ex012.py
@@ -0,0 +1,5 @@
+# Give a discount in a product of 5%
+
+product_price = float(input("Insert the price of a product: "))
+
+print(f"Considering a discount of 5%, the product's price will now be {product_price * (95/100)}")
